e90c5e02635e7ab004f7773329ec8f4e435753e2
Udit Narayan Singh <uditn007@gmail.com>
2025-10-05 10:41:17 +0530

    explored get, post, delete http methods

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,14 +2,25 @@ const express = require('express')
 
 const app = express();
 
-// order is important in express if i put the /ne router before the / route then it works fine
+// This will only handle GET call to the /user
+app.get('/user', (req, res)=>{
+    res.send({
+        'firstName': "udit",
+        'lastName': "ns"
+    })
+})
 
-// If i just use the below code then all the routes have this only whether it is /ne or /anything 
+app.post('/user', (req, res)=>{
+    console.log("Saved the data to the db")
+    res.send("data successfully saved")
+})
 
-// app.use("/",(req, res) => { //nything which matches after "/" the below route will be called 
-//     res.send("hellp dude i am on the server")
-// }) // the text will be shown in all the url like/, /name, /anything
+app.delete('/user', (req, res) => {
+    console.log("user profile deleted successfully")
+    res.send("user is deleted")
+})
 
+// this will handle all the HTTP method to this /test 
 app.use("/test", (req, res) => {
     res.send("hellp dude i am on the server")// only show this in /new
 })
@@ -18,7 +29,3 @@ app.listen(3000, () => {
     console.log("My server is listening on the port 3000")
 })
 
-// To test the api we will not use the browser as it the worst way to test instead we will be using postman
-//In postman we click on 'new' at the left top then select http
-
-// Now we look how we can handle different methods at the same url like in /profile we handle GET, POST, PATCH request
