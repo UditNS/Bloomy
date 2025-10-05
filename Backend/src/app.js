352964fbc501fcd48b27c1cc7d36b4bb06df2468
Udit Narayan Singh <uditn007@gmail.com>
2025-10-05 11:15:48 +0530

    Playing with routes

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -2,24 +2,23 @@ const express = require('express')
 
 const app = express();
 
-// I have put ? after b this means now b is optional. When i cal /abc it will work and also when i call /ac it will also work.
+// To get data from /user?userId=10202&pass=test like userId and pass I can use req.query which will give an object containing userId and pass.
 
-
-// If i put + after b then it means i can add as many b as i want to like abbbbbc, abc, abbbbc but abcc and aaabc won't work
-
-// If i put * after b then it means I can now put anything i want like abJFOIFJOFc, abuditc it will work. It should start with ab and end with c.
-
-// I can group things up like a(bc)?d -> Here bc is optional 
-
-// Regex can also work in path
-
-app.get('/abc', (req, res)=>{
+// dynamic user
+// in path /user/2364782 is dynamic to get dynamic user we use req.params
+// output as 
+// [Object: null prototype] {
+//   userId: '23545',
+//   name: 'udit',
+//   pass: '123adsf'
+// }
+app.get('/user/:userId/:name/:pass', (req, res)=>{
+    console.log(req.params)
     res.send({
         'firstName': "udit",
         'lastName': "ns"
     })
 })
-
 
 
 app.listen(3000, () => {
