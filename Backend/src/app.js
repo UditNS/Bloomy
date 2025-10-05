7217ef07116cad0a37ff38c0ceac44ce0f38c319
Udit Narayan Singh <uditn007@gmail.com>
2025-10-05 21:29:14 +0530

    created a adminAuth middleware and learned how autherization works

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,31 +1,26 @@
 const express = require('express')
 
 const app = express();
+// handling autherization
+const {adminAuth} = require('./middlewares/auth')
 
-// app.get("/route", rh1, rh2, [rh3, rh4], rh5)
-// I can wrap routes inside an array and it works the same
+app.use('/admin', adminAuth)
 
-    // One route can handle multiple route handler
+app.get('/admin/getalluser', (req, res) => {
+    console.log('user autherized')
+    res.send("getting user data");
+})
+
+app.get('/admin/deleteUser', (req, res) => {
+    res.send('deleteing the user')
+})
+
 app.get('/user', (req, res, next)=>{
-    // This function is known as route handler
-    // if we don't send the response back it will be stuck in loop to get the response and gave error as Could not get response
     res.send("Handling route 1")
     console.log("route 1")
     next()
-    
-}, (req, res, next) => {
-    // route handler 2
-    res.send("Handling route 2")
-    console.log("route 2")
-    next()
-},
-(req, res) => {
-    // route handler 2
-    res.send("Handling route 3")
-    console.log("route 3")
 })
 
 app.listen(3000, () => {
     console.log("My server is listening on the port 3000")
 })
-
---
+++ Backend/src/middlewares/auth.js
@@ -0,0 +1,16 @@
+const adminAuth = (req,res,next) => {
+    const token = 'xyz';
+    const isAutherized = token === 'xyz';
+
+    if(!isAutherized){
+        console.log("user not autherized")
+        res.send('user not autherized')
+    }
+    else{
+        next();
+    }
+}
+
+module.exports = {
+    adminAuth
+}
