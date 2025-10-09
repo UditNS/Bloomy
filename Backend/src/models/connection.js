8c1284155a5b88fa96ad0a11304c57470844f605
Udit Narayan Singh <uditn007@gmail.com>
2025-10-09 15:47:52 +0530

    added check onto the request

--- Backend/src/models/connection.js
+++ Backend/src/models/connection.js
@@ -14,7 +14,7 @@ const connectionSchema = new mongoose.Schema({
         required: true,
         // you create a enum when you want to restrict the user to some values
         enum: {
-            values: ["like", "pass", "accepted", "rejected"],
+            values: ["likes", "pass", "accepted", "rejected"],
             // ignored -> pass & intrested -> like
             message: `value is incorrect status type`
         }
@@ -22,7 +22,17 @@ const connectionSchema = new mongoose.Schema({
 }, {
     timestamps : true,
 }
-)
+) 
+// called when ever a new request created
+// this function is kind of a middleware -> calling next()
+connectionSchema.pre("save", function(next) {
+    const connectionRequest = this
+    // now checking when the sender and reciever are the same
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send the connection request to yourself")
+    }
+    next()
+})
 
 const ConnectionModel = new mongoose.model("Connection", connectionSchema)
 
--- Backend/src/routes/requestRouter.js
+++ Backend/src/routes/requestRouter.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const {userAuth} = require("../middlewares/auth");
 const Connection  = require("../models/connection");
+const User = require("../models/user");
 
 const requestRouter = express.Router()
 
@@ -12,9 +13,38 @@ requestRouter.post('/send/:status/:toUserId', userAuth, async (req, res) => {
     try {
         // if i pass in accepted as status -> this mean that toUser accepted the request but it isn't the case.
         // we need to add more strict checks
+        const allowedStatus = ["likes", "pass"]
+
         const fromUserId = req.user._id;
         const toUserId = req.params.toUserId;
         const status = req.params.status;
+
+        // status should be like/ pass
+        if(!allowedStatus.includes(status)){
+            throw new Error(`Invalid status type : ${status}`)
+        }
+        // checking the toUserId should present in db
+        const checkToUser = await User.findById(toUserId);
+        if(!checkToUser){
+            return res.status(400).send({message : "User not exist to which you are trying to send the connection request"})
+        }
+
+        // checking for if there is an existing connection  
+        // a->b already -> show requested
+        // b->a already present -> acccept/ reject
+
+        const alreadyPresent = await Connection.findOne({
+            $or:[ // this is "OR" in mongodb
+                {fromUserId, toUserId},
+                {fromUserId: toUserId, toUserId: fromUserId}
+            ]
+        })
+
+        // also checking a->a. User can't send the request to itself -> schema level using pre
+
+        if(alreadyPresent){
+            return res.status(400).send({message : "request already send/ please accept or reject the request"})
+        }
         
         const connection = new Connection({
             fromUserId,
@@ -25,7 +55,7 @@ requestRouter.post('/send/:status/:toUserId', userAuth, async (req, res) => {
         const data = await connection.save();
         
         res.json({
-            message: "Connection request sent successfully",
+            message: req.user.firstName + ' you ' + status + " " + checkToUser.firstName,
             data,
         });
 
