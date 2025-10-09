6c8fd05d6241673c32070630df15840b1e90db6f
Udit Narayan Singh <uditn007@gmail.com>
2025-10-09 13:12:28 +0530

    connection schema

---
+++ Backend/src/models/connection.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+
+const connectionSchema = new mongoose.Schema({
+    fromUserId : {
+        type : mongoose.Schema.Types.ObjectId, // we are storing the _id
+        required: true,
+    },
+    toUserId : {
+        type : mongoose.Schema.Types.ObjectId, // we are storing the _id
+        required: true,
+    },
+    status:{
+        type: String,
+        required: true,
+        // you create a enum when you want to restrict the user to some values
+        enum: {
+            values: ["accepted", "interested", "rejected", "ignored"],
+            // ignored -> block & intrested -> pending
+            message: `${value} is incorrect status type`
+        }
+    }
+}, {
+    timestamps : true,
+}
+)
+
+const ConnectionModel = new mongoose.model("Connection", connectionSchema)
+
+module.exports = ConnectionModel;
