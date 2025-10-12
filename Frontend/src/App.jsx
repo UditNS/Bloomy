276066d1a2e21cb462ccf95e9f2370c65f2105a0
Udit Narayan Singh <uditn007@gmail.com>
2025-10-12 12:51:24 +0530

    added feed and profile component templete

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,13 +4,16 @@ import { ThemeProvider } from "./components/navBar/ThemeProvider";
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router";
 import Login from "./components/login/Login";
 import Layout from "./Layout";
+import Feed from "./components/feed/Feed";
+import Profile from "./components/profile/Profile";
 
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path = '/' element = {<Layout />}>
-        <Route path = '/login' element = {<Login />}></Route>
-        {/* <Route path = '/profile' element = {<Profile />}></Route> */}
+          <Route path = '/' element = {<Feed />}></Route>
+          <Route path = '/login' element = {<Login />}></Route>
+        <Route path = '/profile' element = {<Profile />}></Route>
         {/* <Route loader={gitInfoLoader} path = '/github' element = {<Github />}></Route> */}
       </Route>
     )
